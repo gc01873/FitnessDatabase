1954a23cfad18425878c46185dc11a5992445c3d
gc01873 <42818728+gc01873@users.noreply.github.com>
2020-04-20 19:30:53 -0400

    Add files via upload

---
+++ Fit_DB.sql
@@ -0,0 +1,65 @@
+create database Fit_DB;
+use Fit_DB;
+
+/*BMI will be a derived attribute later on */ 
+create Table Client(
+id int not null,
+fname varchar(20) not null,
+lname varchar(20) not null,
+weight int,
+height int,
+BMI int,
+experience int not null,
+trainerID int,
+PRIMARY KEY(id),
+FOREIGN KEY trainerID REFERENCES Trainer(id),
+Constraint CHECK (REGEXP_LIKE(experience,'0|1|2|3','i'))
+)
+
+
+/*Intensity, if not null must correclate with client experience*/
+/* Client has  a  1 to many relationship with targetArea*/
+create Table TargetArea(
+area varchar(25) not null,
+intensity int,
+category varchar(20),
+Constraint CHECK (REGEXP_LIKE(category,'aerobic|weight-lifting|bodyweight exercises|balance','i'))
+)
+
+/*Must find a way to add cardinality*/
+create Table Exercise(
+nameOfExercise varchar(30) not null,
+area varchar(25),
+difficulty int,
+category varchar(20),
+FOREIGN KEY category REFERENCES TargetArea(category)
+)
+
+/*Should have some kind of cardinality*/
+create Table Trainer(
+id int not null,
+clientID int,
+fname varchar(20) not null,
+lname varchar(20) not null,
+expertise varchar(25),
+PRIMARY KEY(id)
+FOREIGN KEY clientID REFERENCES Client(id)
+
+)
+
+create Table TrainerClientID(
+trainerID int not null,
+clientID int,
+FOREIGN KEY trainerID REFERENCES Trainer(id),
+FOREIGN KEY clientID REFERENCES Client(id)
+
+)
+
+create Table Goals(
+clientID int not null,
+targetBMI int,
+targetWeight int,
+FOREIGN KEY clientID REFERENCES Client(id)
+)
+
+
