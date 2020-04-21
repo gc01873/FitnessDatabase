7207027133c47da99490d4bca036187f6f37654f
zackbutler13 <42818530+zackbutler13@users.noreply.github.com>
2020-04-20 22:45:45 -0400

    Add files via upload
    Changed schema

--- Fit_DB.sql
+++ Fit_DB.sql
@@ -10,11 +10,9 @@ weight int,
 height int,
 BMI int,
 experience int not null,
-trainerID int,
 PRIMARY KEY(id),
-FOREIGN KEY trainerID REFERENCES Trainer(id),
 check(experience >=0 and experience <=4)
-)
+);
 
 
 /*Intensity, if not null must correclate with client experience*/
@@ -22,44 +20,38 @@ check(experience >=0 and experience <=4)
 create Table TargetArea(
 area varchar(25) not null,
 intensity int,
-category varchar(20),
-Constraint CHECK (REGEXP_LIKE(category,'aerobic|weight-lifting|bodyweight exercises|balance','i'))
-)
+category ENUM('aerobic','weight-lifting','bodyweight','balance'),
+primary key(area)
+);
 
 /*Must find a way to add cardinality*/
 create Table Exercise(
 nameOfExercise varchar(30) not null,
-area varchar(25),
+area varchar(25) not null,
 difficulty int,
-category varchar(20),
-FOREIGN KEY category REFERENCES TargetArea(category)
-)
+category ENUM('aerobic','weight-lifting','bodyweight','balance'),
+FOREIGN KEY(area) REFERENCES TargetArea(area)
+);
 
 /*Should have some kind of cardinality*/
 create Table Trainer(
 id int not null,
-clientID int,
 fname varchar(20) not null,
 lname varchar(20) not null,
 expertise varchar(25),
 PRIMARY KEY(id)
-FOREIGN KEY clientID REFERENCES Client(id)
-
-)
+);
 
 create Table TrainerClientID(
 trainerID int not null,
 clientID int,
-FOREIGN KEY trainerID REFERENCES Trainer(id),
-FOREIGN KEY clientID REFERENCES Client(id)
-
-)
+FOREIGN KEY(trainerID) REFERENCES Trainer(id),
+FOREIGN KEY(clientID) REFERENCES Client(id)
+);
 
 create Table Goals(
 clientID int not null,
 targetBMI int,
 targetWeight int,
-FOREIGN KEY clientID REFERENCES Client(id)
-)
-
-
+FOREIGN KEY(clientID) REFERENCES Client(id)
+);
